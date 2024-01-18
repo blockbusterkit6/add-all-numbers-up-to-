ffac7ac2458e2b169a0eed06963a034546353c4f
blockbusterkit6 <47342798+blockbusterkit6@users.noreply.github.com>
2024-01-18 11:34:12 -0600

    Add files via upload

---
+++ add to 1000.py
@@ -0,0 +1,5 @@
+total = 0
+for num in range(1001):#this number +1 is the number you are adding to
+    total = total + num
+print('your total is ' + str(total))
+total = input()#so window will not close
